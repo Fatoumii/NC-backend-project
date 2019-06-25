248e2f299e856929a07f307c0e519b0367b07b04
Fatimah <fatimah@Fatimahs-MBP.broadband>
2019-06-25 19:09:22 +0100

    added topic as key before object passes for first endpoint

--- controller/topicsController.js
+++ controller/topicsController.js
@@ -3,16 +3,10 @@ const { fetchTopics } = require("../models/topicsModels");
 function getTopics(req, res, next) {
   fetchTopics()
     .then(topics => {
-      res.status(200).send({ topics });
+      res.status(200).send({ topics: topics });
     })
     .catch(next);
 }
 module.exports = { getTopics };
 
-/*
-{ 
-  "topics": [
-  {"description": "Code is love, code is life", "slug": "coding"
-  }
-]
-*/
+//test topic key?
