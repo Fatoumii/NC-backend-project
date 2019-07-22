a6456290b89074390d49b808e7abc3f1180f9716
Fatimah <fatimah@Fatimahs-MacBook-Pro.local>
2019-07-22 12:12:03 +0100

    removed console logs

--- errors/index.js
+++ errors/index.js
@@ -22,7 +22,6 @@ exports.handlesCustomErrors = (err, req, res, next) => {
 };
 
 exports.handles500Errors = (err, req, res, next) => {
-  console.log(err);
   res.status(500).send({ msg: "Internal server error" });
 };
 
