5376ba44d2ab9ac59f26d2949bb9876517464638
Fatimah <fatimah@Fatimahs-MacBook-Pro.local>
2019-07-22 12:02:11 +0100

    added test env in spec file

--- controller/usersController.js
+++ controller/usersController.js
@@ -2,6 +2,7 @@ const { fetchUsers } = require("../models/usersModels");
 
 function getUsers(req, res, next) {
   const { username } = req.params;
+  console.log(username);
   fetchUsers(username)
     .then(([userObj]) => {
       res.status(200).send({ user: userObj });
--- models/usersModels.js
+++ models/usersModels.js
@@ -1,11 +1,13 @@
 const connection = require("../db/connection");
 
 function fetchUsers(username) {
+  console.log(username, "username");
   return connection
     .select("*")
     .from("users")
     .where("username", "=", username)
     .then(user => {
+      console.log(user);
       if (user.length === 0) {
         return Promise.reject({ status: 404, msg: "User not found" });
       }
